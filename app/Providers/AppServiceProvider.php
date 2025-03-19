3741b4236c43c39eb60cd593982ed22765ddfbd5
Raihan Alkawsar <tlod9906@gmail.com>
2025-03-19 15:00:00 +0700

    feat(providers): add extension checker in service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Carbon\Carbon;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,7 +23,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        config(['app.locale' => 'id']);
         Carbon::setLocale('id');
+        RateLimiter::for('login', function (Request $request) {
+            return [
+                Limit::perMinute(500),
+                Limit::perMinute(3)->by($request->input('email')),
+            ];
+        });
+        RateLimiter::for('global', function (Request $request) {
+            return Limit::perMinute(1000);
+        });
     }
 }
