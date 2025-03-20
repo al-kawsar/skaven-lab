1e86041e03b8b3eb21b1e9d8b3ac562a3d74362f
Raihan Alkawsar <tlod9906@gmail.com>
2025-03-20 18:13:44 +0800

    feat: tambahkan helper untuk format angka

---
+++ app/Helpers/NumberFormatterHelper.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Helpers;
+
+class NumberFormatterHelper
+{
+    public static function format($number)
+    {
+        if ($number >= 1000000000000) {
+            return number_format($number / 1_000_000_000_000, 1) . 'T';
+        }
+
+        if ($number >= 1000000000) {
+            return number_format($number / 1_000_000_000, 1) . 'M';
+        }
+
+        if ($number >= 1000000) {
+            return number_format($number / 1_000_000, 1) . 'jt';
+        }
+
+        if ($number >= 1000) {
+            return number_format($number / 1_000, 1) . 'rb';
+        }
+
+        return $number;
+    }
+}
