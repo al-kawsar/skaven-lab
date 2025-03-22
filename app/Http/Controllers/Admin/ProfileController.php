d1789e82ddb842c91f3b88c42d47b16dd591b5ac
Raihan Alkawsar <tlod9906@gmail.com>
2025-03-22 21:58:41 +0800

    refactor: update ProfileController with improved validation

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -10,7 +10,7 @@ class ProfileController extends Controller
 {
     public function index()
     {
-        return view('pages.admin.profile.index');
+        return view('pages.profile.index');
     }
 
 
