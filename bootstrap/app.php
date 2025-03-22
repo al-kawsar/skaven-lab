83d40120206b7795334cb666e7058220474beb32
Raihan Alkawsar <tlod9906@gmail.com>
2025-03-22 21:59:52 +0800

    refactor: update bootstrap app with improved middleware handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Middleware\EnsureUserHasRole;
+use App\Http\Middleware\SecurityHeaders;
 use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
@@ -12,8 +14,17 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->redirectGuestsTo(fn(Request $request) => route('auth.login'));
+        $middleware->web([
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            SecurityHeaders::class, // Add security headers to all responses
+        ]);
         $middleware->alias([
-            'role' => EnsureUserHasRole::class
+            'role' => EnsureUserHasRole::class,
+            'Excel' => Maatwebsite\Excel\Facades\Excel::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
