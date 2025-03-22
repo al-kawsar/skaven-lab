51695096eb0a7b754f114371ebf3dcddc56e16c4
Raihan Alkawsar <tlod9906@gmail.com>
2025-03-22 21:58:36 +0800

    feat: add students table migration

---
+++ database/migrations/2024_06_24_060159_create_students_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('nis');
+            $table->string('nisn');
+            $table->string('alamat');
+            $table->enum('jenis_kelamin',['l','p']);
+            $table->string('agama');
+            $table->string('foto_siswa')->nullable(true);
+            $table->date('tgl_lahir');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists(table: 'students');
+    }
+};
