fc7b3cefa69edf37333a01c72ff4ef3ae133da0a
Raihan Alkawsar <tlod9906@gmail.com>
2025-03-22 21:58:38 +0800

    refactor: update User model relationships and remove deprecated methods

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,13 +8,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Scout\Searchable;
 
 class User extends Authenticatable
 {
     use HasFactory, Notifiable, HasRole;
 
-    public $timestamps = false;
-
+    protected $keyType = 'string';
+    public $incrementing = false;
     protected $fillable = [
         'name',
         'email',
@@ -25,6 +26,29 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
     ];
+
+    const ROLE_SUPERADMIN = 1;
+    const ROLE_ADMIN = 2;
+    const ROLE_TEACHER = 3;
+    const ROLE_STUDENT = 4;
+    const ROLE_GUEST = 5;
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->id = (string) str()->uuid(); // Menghasilkan UUID saat membuat model
+        });
+    }
+    public function toSearchableArray()
+    {
+        return [
+            'id' => (int) $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+        ];
+    }
 
     protected function casts(): array
     {
@@ -38,8 +62,8 @@ class User extends Authenticatable
         return $this->belongsTo(Role::class, 'role_id');
     }
 
-    public function file()
-    {
-        return $this->hasOne(File::class, 'id');
-    }
+    // public function file()
+    // {
+    //     return $this->hasOne(File::class, 'id');
+    // }
 }
