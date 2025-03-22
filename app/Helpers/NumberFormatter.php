7caec87b4f7cd4bc531c842f0662041989d50e04
Raihan Alkawsar <tlod9906@gmail.com>
2025-03-22 21:59:50 +0800

    refactor: update NumberFormatter helper with improved methods

--- app/Helpers/NumberFormatter.php
+++ app/Helpers/NumberFormatter.php
@@ -6,14 +6,22 @@ class NumberFormatter
 {
     public static function format($number)
     {
+        if ($number >= 1000000000000) {
+            return number_format($number / 1_000_000_000_000, 1) . 'T';
+        }
+
+        if ($number >= 1000000000) {
+            return number_format($number / 1_000_000_000, 1) . 'M';
+        }
+
         if ($number >= 1000000) {
-            return number_format($number / 1000000, 1) . 'jt'; // Format untuk juta
+            return number_format($number / 1_000_000, 1) . 'jt';
         }
 
         if ($number >= 1000) {
-            return number_format($number / 1000, 1) . 'rb'; // Format untuk ribu
+            return number_format($number / 1_000, 1) . 'rb';
         }
 
-        return $number; // Jika kurang dari 1000, tampilkan angka aslinya
+        return $number;
     }
 }
