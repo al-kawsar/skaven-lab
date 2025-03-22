a98bdf7d336d29d19c661c59a75aa2172a1938e8
Raihan Alkawsar <tlod9906@gmail.com>
2025-03-22 21:59:51 +0800

    refactor: update AppServiceProvider with improved service bindings

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\Blade;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -33,5 +34,9 @@ class AppServiceProvider extends ServiceProvider
         RateLimiter::for('global', function (Request $request) {
             return Limit::perMinute(1000);
         });
+
+        // Load the custom CSS file
+        Blade::component('image-uploader', \App\View\Components\ImageUploader::class);
+        Blade::component('image-preview', \App\View\Components\ImagePreview::class);
     }
 }
