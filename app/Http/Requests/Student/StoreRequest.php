26ac0e5b0037ebe5603cffcdda1a42b3fbd14f2a
Raihan Alkawsar <tlod9906@gmail.com>
2025-03-22 21:58:35 +0800

    feat: add new Student request validators with improved validation

---
+++ app/Http/Requests/Student/StoreRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests\Student;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return auth()->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|max:255|string',
+            'nis' => 'required',
+            'nisn' => 'required',
+            'alamat' => 'required|max:255|string',
+            'jenis_kelamin' => 'required|max:255',
+            'agama' => 'required|max:255|string',
+            'tgl_lahir' => 'required|max:255|date_format:d-m-Y',
+            'foto_siswa' => 'sometimes|file|image|required|max:255|mimes:png,jpg,jpeg,webp,png'
+        ];
+    }
+}
