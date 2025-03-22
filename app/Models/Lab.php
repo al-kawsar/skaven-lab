61b8cad1b01ebb4495171f40b5f70e0330e63f25
Raihan Alkawsar <tlod9906@gmail.com>
2025-03-22 21:58:38 +0800

    refactor: update Lab model with improved relationship definitions

--- app/Models/Lab.php
+++ app/Models/Lab.php
@@ -9,12 +9,41 @@ use App\Models\File;
 class Lab extends Model
 {
     use HasFactory;
+    protected $keyType = 'string';
+    public $incrementing = false;
+
 
     protected $guarded = ['id'];
 
     protected $dates = ['created_at', 'updated_at'];
 
-    public function file() {
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->id = (string) str()->uuid(); // Menghasilkan UUID saat membuat model
+        });
+    }
+
+    public function file()
+    {
         return $this->belongsTo(File::class, 'thumbnail');
+    }
+
+    public function users()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function borrowings()
+    {
+        return $this->hasMany(Borrowing::class, 'lab_id', 'id');
+    }
+
+    // Add new relationship for slider images
+    public function sliderImages()
+    {
+        return $this->hasMany(LabSlider::class, 'lab_id', 'id');
     }
 }
