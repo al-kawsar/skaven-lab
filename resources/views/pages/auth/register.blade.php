7bf0397cddf6437cc5793869b3bc396d8d45ad76
Raihan Alkawsar <tlod9906@gmail.com>
2025-03-22 21:59:22 +0800

    refactor: update register page with enhanced form validation

--- resources/views/pages/auth/register.blade.php
+++ resources/views/pages/auth/register.blade.php
@@ -1,34 +1,52 @@
 @extends('layouts.auth-layout')
+@section('title', 'Register')
+
 @section('auth-content')
     <div class="loginbox">
         <div class="login-left">
             <img class="img-fluid" src="/assets/img/login.png" alt="Logo">
         </div>
+
         <div class="login-right">
             <div class="login-right-wrap">
                 <h1>Sign Up</h1>
                 <p class="account-subtitle">Enter details to create your account</p>
-
-                <form action="login.html">
+                <form method="POST" action="{{ route('auth.doRegister') }}">
+                    @csrf
+                    @method('POST')
                     <div class="form-group">
                         <label>Name <span class="login-danger">*</span></label>
-                        <input class="form-control" type="text">
+                        <input class="form-control" type="text" autofocus value="{{ old('name') }}" name="name">
                         <span class="profile-views"><i class="fas fa-user-circle"></i></span>
+                        @error('name')
+                            <div class="text-danger text-lowercase">{{ $message }}</div>
+                        @enderror
                     </div>
                     <div class="form-group">
                         <label>Email <span class="login-danger">*</span></label>
-                        <input class="form-control" type="text">
+                        <input class="form-control" type="email" value="{{ old('email') }}" name="email">
                         <span class="profile-views"><i class="fas fa-envelope"></i></span>
+                        @error('email')
+                            <div class="text-danger text-lowercase">{{ $message }}</div>
+                        @enderror
                     </div>
                     <div class="form-group">
                         <label>Password <span class="login-danger">*</span></label>
-                        <input class="form-control pass-input" type="password">
+                        <input class="form-control pass-input" type="password" value="{{ old('password') }}"
+                            name="password">
                         <span class="profile-views feather-eye-off toggle-password"></span>
+                        @error('password')
+                            <div class="text-danger text-lowercase">{{ $message }}</div>
+                        @enderror
                     </div>
                     <div class="form-group">
                         <label>Confirm password <span class="login-danger">*</span></label>
-                        <input class="form-control pass-confirm" type="text">
-                        <span class="profile-views feather-eye reg-toggle-password"></span>
+                        <input class="form-control pass-confirm" type="password" value="{{ old('password_confirmation') }}"
+                            name="password_confirmation">
+                        <span class="profile-views feather-eye-off toggle-password"></span>
+                        @error('password_confirmation')
+                            <div class="text-danger text-lowercase">{{ $message }}</div>
+                        @enderror
                     </div>
                     <div class=" dont-have">Sudah Punya akun? <a href="{{ route('auth.login') }}">Login</a></div>
                     <div class="form-group mb-0">
