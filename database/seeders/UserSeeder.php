970be16f7a95b06a126fd9734064d30142ee27c5
Raihan Alkawsar <tlod9906@gmail.com>
2025-03-22 22:01:00 +0800

    refactor: improve UserSeeder with better test data generation

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +13,33 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'SuperAdmin - SMKN 7 Makassar',
+            'email' => 'superadmin@belajar.id',
+            'password' => bcrypt('123123123'),
+            'role_id' => User::ROLE_SUPERADMIN
+        ]);
+
+        User::create([
+            'name' => 'Staff Admin - SMKN 7 Makassar',
+            'email' => 'admin@smk7.sch.id',
+            'password' => bcrypt('123123123'),
+            'role_id' => User::ROLE_ADMIN
+        ]);
+
+        User::create([
+            'name' => 'Ibrahim Mallombassang',
+            'email' => 'ibrahim@smk7.sch.id',
+            'password' => bcrypt('123123123'),
+            'role_id' => User::ROLE_TEACHER
+        ]);
+
+        User::create([
+            'name' => 'siswa',
+            'email' => 'andimuhraihan22355@smk7.sch.id',
+            'password' => bcrypt('123123123'),
+            'role_id' => User::ROLE_STUDENT
+        ]);
     }
 }
