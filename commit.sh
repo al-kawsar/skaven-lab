b8c9288afd474455f9ec9d6d5b2414607f0b185f
Raihan Alkawsar <tlod9906@gmail.com>
2025-03-26 18:25:37 +0800

    feat: tambah file commit.sh

---
+++ commit.sh
@@ -0,0 +1,179 @@
+git add public/assets/plugins/icons/feather/fonts/assets/css/assets/img/image-04.jpg.html
+git commit -m "chore: hapus file image-04.jpg.html" --date="2025-03-25"
+
+git add public/assets/plugins/icons/feather/fonts/assets/css/assets/img/image-05.jpg.html
+git commit -m "chore: hapus file image-05.jpg.html" --date="2025-03-25"
+
+git add public/assets/plugins/icons/feather/fonts/assets/css/assets/img/image-06.jpg.html
+git commit -m "chore: hapus file image-06.jpg.html" --date="2025-03-25"
+
+git add public/assets/plugins/icons/feather/fonts/assets/css/assets/img/image-07.jpg.html
+git commit -m "chore: hapus file image-07.jpg.html" --date="2025-03-25"
+
+git add public/assets/plugins/icons/feather/fonts/assets/css/assets/img/image-08.jpg.html
+git commit -m "chore: hapus file image-08.jpg.html" --date="2025-03-25"
+
+git add public/assets/plugins/icons/feather/fonts/assets/css/assets/img/image-09.jpg.html
+git commit -m "chore: hapus file image-09.jpg.html" --date="2025-03-25"
+
+git add public/assets/plugins/icons/feather/fonts/assets/css/assets/img/image-1.png.html
+git commit -m "chore: hapus file image-1.png.html" --date="2025-03-25"
+
+
+# Stage and commit deleted files
+git add routes/app/profile.php
+git commit -m "chore: hapus file profile.php" --date="2025-03-26"
+
+git add routes/app/settings.php
+git commit -m "chore: hapus file settings.php" --date="2025-03-26"
+
+git add routes/app/teacher.php
+git commit -m "chore: hapus file teacher.php" --date="2025-03-26"
+
+# Stage and commit untracked files
+git add app/Exports/EquipmentExport.php
+git commit -m "feat: tambah file EquipmentExport.php" --date="2025-03-26"
+
+git add app/Helpers/ReportHelper.php
+git commit -m "feat: tambah file ReportHelper.php" --date="2025-03-26"
+
+git add app/Helpers/UserHelper.php
+git commit -m "feat: tambah file UserHelper.php" --date="2025-03-26"
+
+git add app/Helpers/settings.php
+git commit -m "feat: tambah file settings.php" --date="2025-03-26"
+
+git add app/Http/Controllers/Borrowing/ReturnController.php
+git commit -m "feat: tambah file ReturnController.php" --date="2025-03-26"
+
+git add app/Http/Controllers/Inventory/EquipmentCategoryController.php
+git commit -m "feat: tambah file EquipmentCategoryController.php" --date="2025-03-26"
+
+git add app/Http/Controllers/Inventory/EquipmentLocationController.php
+git commit -m "feat: tambah file EquipmentLocationController.php" --date="2025-03-26"
+
+git add app/Http/Controllers/Lab/LabController.php
+git commit -m "feat: tambah file LabController.php" --date="2025-03-26"
+
+git add app/Http/Controllers/Lab/LabScheduleController.php
+git commit -m "feat: tambah file LabScheduleController.php" --date="2025-03-26"
+
+git add app/Http/Controllers/Report/InventoryReportController.php
+git commit -m "feat: tambah file InventoryReportController.php" --date="2025-03-26"
+
+git add app/Http/Controllers/Report/LabBorrowingReportController.php
+git commit -m "feat: tambah file LabBorrowingReportController.php" --date="2025-03-26"
+
+git add app/Http/Controllers/Setting/GeneralSettingController.php
+git commit -m "feat: tambah file GeneralSettingController.php" --date="2025-03-26"
+
+git add app/Http/Controllers/Setting/SecuritySettingController.php
+git commit -m "feat: tambah file SecuritySettingController.php" --date="2025-03-26"
+
+git add app/Http/Controllers/Setting/SettingController.php
+git commit -m "feat: tambah file SettingController.php" --date="2025-03-26"
+
+git add app/Http/Controllers/User/StudentController.php
+git commit -m "feat: tambah file StudentController.php" --date="2025-03-26"
+
+git add app/Http/Controllers/User/TeacherController.php
+git commit -m "feat: tambah file TeacherController.php" --date="2025-03-26"
+
+git add app/Http/Requests/Profile/UpdateRequest.php
+git commit -m "feat: tambah file UpdateRequest.php" --date="2025-03-26"
+
+git add app/Imports/EquipmentImport.php
+git commit -m "feat: tambah file EquipmentImport.php" --date="2025-03-26"
+
+git add app/Models/Student.php
+git commit -m "feat: tambah file Student.php" --date="2025-03-26"
+
+git add app/Providers/SettingsServiceProvider.php
+git commit -m "feat: tambah file SettingsServiceProvider.php" --date="2025-03-26"
+
+git add app/Repositories/EquipmentLocationRepository.php
+git commit -m "feat: tambah file EquipmentLocationRepository.php" --date="2025-03-26"
+
+git add app/Services/Inventory/EquipmentLocationService.php
+git commit -m "feat: tambah file EquipmentLocationService.php" --date="2025-03-26"
+
+git add app/Traits/Lab/ImageHandlerTrait.php
+git commit -m "feat: tambah file ImageHandlerTrait.php" --date="2025-03-26"
+
+git add app/Transformers/EquipmentLocationTransformer.php
+git commit -m "feat: tambah file EquipmentLocationTransformer.php" --date="2025-03-26"
+
+git add app/Transformers/EquipmentTransformer.php
+git commit -m "feat: tambah file EquipmentTransformer.php" --date="2025-03-26"
+
+git add app/Transformers/LabTransformer.php
+git commit -m "feat: tambah file LabTransformer.php" --date="2025-03-26"
+
+# Stage and commit other untracked files
+git add commit.sh
+git commit -m "feat: tambah file commit.sh" --date="2025-03-26"
+
+git add config/sanctum.php
+git commit -m "feat: tambah file sanctum.php" --date="2025-03-26"
+
+git add config/tinker.php
+git commit -m "feat: tambah file tinker.php" --date="2025-03-26"
+
+git add database/factories/EquipmentBorrowingFactory.php
+git commit -m "feat: tambah file EquipmentBorrowingFactory.php" --date="2025-03-26"
+
+git add database/factories/EquipmentCategoryFactory.php
+git commit -m "feat: tambah file EquipmentCategoryFactory.php" --date="2025-03-26"
+
+git add database/factories/EquipmentLocationFactory.php
+git commit -m "feat: tambah file EquipmentLocationFactory.php" --date="2025-03-26"
+
+git add database/factories/LabBorrowingHistoryFactory.php
+git commit -m "feat: tambah file LabBorrowingHistoryFactory.php" --date="2025-03-26"
+
+git add database/migrations/2025_04_02_181304_create_equipment_locations_table.php
+git commit -m "feat: tambah migration create_equipment_locations_table.php" --date="2025-03-26"
+
+git add database/migrations/2025_04_02_181314_create_equipment_categories_table.php
+git commit -m "feat: tambah migration create_equipment_categories_table.php" --date="2025-03-26"
+
+git add database/migrations/2025_04_02_181339_create_equipment_borrowings_table.php
+git commit -m "feat: tambah migration create_equipment_borrowings_table.php" --date="2025-03-26"
+
+git add database/migrations/2026_04_02_180821_create_equipment_table.php
+git commit -m "feat: tambah migration create_equipment_table.php" --date="2025-03-26"
+
+git add database/seeders/EquipmentBorrowingSeeder.php
+git commit -m "feat: tambah seeder EquipmentBorrowingSeeder.php" --date="2025-03-26"
+
+git add database/seeders/EquipmentCategorySeeder.php
+git commit -m "feat: tambah seeder EquipmentCategorySeeder.php" --date="2025-03-26"
+
+git add database/seeders/EquipmentLocationSeeder.php
+git commit -m "feat: tambah seeder EquipmentLocationSeeder.php" --date="2025-03-26"
+
+git add database/seeders/LabBorrowingHistorySeeder.php
+git commit -m "feat: tambah seeder LabBorrowingHistorySeeder.php" --date="2025-03-26"
+
+git add docker/8.0/php.ini
+git commit -m "feat: tambah php.ini untuk docker 8.0" --date="2025-03-26"
+
+git add docker/8.0/start-container
+git commit -m "feat: tambah start-container untuk docker 8.0" --date="2025-03-26"
+
+git add docker/8.0/supervisord.conf
+git commit -m "feat: tambah supervisord.conf untuk docker 8.0" --date="2025-03-26"
+
+git add docker/8.1/php.ini
+git commit -m "feat: tambah php.ini untuk docker 8.1" --date="2025-03-26"
+
+git add docker/8.1/start-container
+git commit -m "feat: tambah start-container untuk docker 8.1" --date="2025-03-26"
+
+git add docker/8.1/supervisord.conf
+git commit -m "feat: tambah supervisord.conf untuk docker 8.1" --date="2025-03-26"
+
+git add docker/8.2/Dockerfile
+git commit -m "feat: tambah Dockerfile untuk docker 8.2" --date="2025-03-26"
+
+# (continue with the rest of the files similarly)
