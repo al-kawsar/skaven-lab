b2a88c2933c6713c77f7f1ac9ed1076eebac3dcf
Raihan Alkawsar <tlod9906@gmail.com>
2025-04-03 17:49:36 +0800

    fix: update 405 error page

--- resources/views/errors/405.blade.php
+++ resources/views/errors/405.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="id">
 
 <head>
     <meta charset="utf-8">
@@ -8,8 +8,10 @@
 
     <link rel="shortcut icon" href="/assets/img/favicon.png">
 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
-        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500;1,700&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet">
 
     <link rel="stylesheet" href="/assets/plugins/bootstrap/css/bootstrap.min.css">
@@ -31,7 +33,7 @@
             <h1>405</h1>
             <h3 class="h2 mb-3"><i class="fas fa-exclamation-triangle"></i> Ups! Halaman tidak ditemukan!</h3>
             <p class="h4 font-weight-normal">Halaman yang Anda minta tidak di temukan.</p>
-            <a href="/" class="btn btn-primary">Back to Home</a>
+            <a href="{{ url()->previous() }}" class="btn btn-primary">Back to Home</a>
         </div>
     </div>
 
