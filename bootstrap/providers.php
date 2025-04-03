f35a2712bcf6a69716e410b91cbac49bb8f7514e
Raihan Alkawsar <tlod9906@gmail.com>
2025-04-03 17:48:58 +0800

    chore: update bootstrap providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,5 +2,10 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\HelperServiceProvider::class,
+    App\Providers\SettingsServiceProvider::class,
+    App\Providers\HelperServiceProvider::class,
+
+    
     Maatwebsite\Excel\ExcelServiceProvider::class,
 ];
