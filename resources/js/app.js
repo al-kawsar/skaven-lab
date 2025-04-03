36437adddbc518af557dc4e1dba14d6d0a8868c9
Raihan Alkawsar <tlod9906@gmail.com>
2025-04-03 17:49:32 +0800

    feat: update main JavaScript file

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
-import './bootstrap';
+import "./bootstrap";
+
+// Import library NProgress
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+
+// Show the progress bar
+NProgress.start();
