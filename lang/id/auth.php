225ce7bc3be0eae4bc545f39dc180236752ba0c2
Raihan Alkawsar <tlod9906@gmail.com>
2025-04-03 17:49:18 +0800

    fix: update Indonesian auth language file

--- lang/id/auth.php
+++ lang/id/auth.php
@@ -1,20 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used during authentication for various
-    | messages that we need to display to the user. You are free to modify
-    | these language lines according to your application's requirements.
-    |
-    */
-
-    'failed' => "Identitas tersebut tidak cocok dengan data kami.",,
-    'password' => "Kata sandi salah.",
+    'failed'   => 'Identitas tersebut tidak cocok dengan data kami.',
+    'password' => 'Kata sandi salah.',
     'throttle' => 'Terlalu banyak upaya masuk. Silahkan coba lagi dalam :seconds detik.',
-
 ];
