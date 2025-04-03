ebcc21e658777737cd480e173f7d742636c033d4
Raihan Alkawsar <tlod9906@gmail.com>
2025-04-03 17:49:57 +0800

    chore: update user routes

--- routes/app/user.php
+++ routes/app/user.php
@@ -1,56 +1,78 @@
 <?php
-use App\Http\Controllers\Admin\TeacherController;
-use App\Http\Controllers\Admin\StudentController;
+
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\User\TeacherController;
+use App\Http\Controllers\User\StudentController;
+use App\Http\Controllers\User\UserController;
+use App\Http\Controllers\User\ProfileController;
 
-// * User Resource
-Route::controller(UserController::class)->name('admin.user.')->prefix('admin/users')->middleware(['role:superadmin,admin', 'auth'])->group(function () {
-    Route::get('/', 'index')->name('index');
-    Route::get('/api/get-data', 'getData')->name('get-data');
-    Route::get('/add', 'create')->name('create');
-    Route::get('/{user:id}', 'show')->name('show');
-    Route::post('/', 'store')->name('store');
-    Route::get('/{user:id}/edit', 'edit')->name('edit');
-    Route::put('{user:id}', 'update')->name('update');
-    Route::delete('/delete_all', 'destroyAll')->name('destroy.all');
-    Route::delete('/{id:id}', 'destroy')->name('destroy');
-});
+/*
+|--------------------------------------------------------------------------
+| User Management Routes
+|--------------------------------------------------------------------------
+*/
 
-// * Student Resource
-Route::controller(StudentController::class)->name('admin.student.')->prefix('admin/students')->middleware(['role:superadmin,admin', 'auth'])->group(function () {
-    Route::get('/', 'index')->name('index');
-    Route::get('/api/get-data', 'getData')->name('get-data');
-    Route::get('/add', 'create')->name('create');
+Route::middleware(['auth'])->group(function () {
 
-    // ? The statistics routes should appear BEFORE the show route with the parameter
-    Route::get('/statistics/data', 'getStatistics')->name('statistics');
-    Route::get('/statistics/view', 'statisticsView')->name('statistics.view');
+    // User profile
+    Route::controller(ProfileController::class)->name('profile.')->prefix('profile')->group(function () {
+        Route::get('/', 'index')->name('index');
+        Route::put('/', 'update')->name('update');
+    });
 
-    // ? Export & Import routes
-    Route::get('/export/excel', 'exportToExcel')->name('export.excel');
-    Route::post('/import', 'importFromExcel')->name('import');
-    Route::get('/import/template', 'downloadImportTemplate')->name('import.template');
-
-    // ? These routes with model binding should be AFTER the specific routes
-    Route::get('/{siswa}', 'show')->name('show');
-    Route::get('/{siswa}/edit', 'edit')->name('edit');
-    Route::put('/{siswa}', 'update')->name('update');
-    Route::post('/', 'store')->name('store');
-    Route::delete('/delete_all', 'destroyAll')->name('destroy.all');
-    Route::delete('/{siswa}', 'destroy')->name('destroy');
-});
+    Route::controller(ProfileController::class)->name('admin.report.')->prefix('report')->middleware('role:superadmin,admin')->group(function () {
+        Route::get('/', 'index')->name('index');
+        Route::get('/s', 'index')->name('generate');
+    });
 
 
-// * Teacher Resource
-Route::controller(TeacherController::class)->name('admin.teacher.')->prefix('admin/teachers')->middleware(['role:superadmin,admin', 'auth'])->group(function () {
-    Route::get('/', 'index')->name('index');
-    Route::get('/api/get-data', 'getData')->name('get-data');
-    Route::get('/add', 'create')->name('create');
-    Route::get('/{user:id}', 'show')->name('show');
-    Route::post('/', 'store')->name('store');
-    Route::get('/{user:id}/edit', 'edit')->name('edit');
-    Route::put('{user:id}', 'update')->name('update');
-    Route::delete('/delete_all', 'destroyAll')->name('destroy.all');
-    Route::delete('/{id:id}', 'destroy')->name('destroy');
+
+    // * User Resource
+    Route::controller(UserController::class)->name('user.')->prefix('users')->middleware(['role:superadmin,admin', 'auth'])->group(function () {
+        Route::get('/', 'index')->name('index');
+        Route::get('/add', 'create')->name('create');
+        Route::get('/{user:id}', 'show')->name('show');
+        Route::post('/', 'store')->name('store');
+        Route::get('/{user:id}/edit', 'edit')->name('edit');
+        Route::put('{user:id}', 'update')->name('update');
+        Route::delete('/delete_all', 'destroyAll')->name('destroy.all');
+        Route::delete('/{id:id}', 'destroy')->name('destroy');
+    });
+
+    // * Student Resource
+    Route::controller(StudentController::class)->name('student.')->prefix('students')->middleware(['role:superadmin,admin', 'auth'])->group(function () {
+        Route::get('/', 'index')->name('index');
+        Route::get('/add', 'create')->name('create');
+
+        // ? The statistics routes should appear BEFORE the show route with the parameter
+        Route::get('/statistics/data', 'getStatistics')->name('statistics');
+        Route::get('/statistics/view', 'statisticsView')->name('statistics.view');
+
+        // ? Export & Import routes
+        Route::get('/export/excel', 'exportToExcel')->name('export.excel');
+        Route::post('/import', 'importFromExcel')->name('import');
+        Route::get('/import/template', 'downloadImportTemplate')->name('import.template');
+
+        // ? These routes with model binding should be AFTER the specific routes
+        Route::get('/{siswa}', 'show')->name('show');
+        Route::get('/{siswa}/edit', 'edit')->name('edit');
+        Route::put('/{siswa}', 'update')->name('update');
+        Route::post('/', 'store')->name('store');
+        Route::delete('/delete_all', 'destroyAll')->name('destroy.all');
+        Route::delete('/{siswa}', 'destroy')->name('destroy');
+    });
+
+
+    // * Teacher Resource
+    Route::controller(TeacherController::class)->name('teacher.')->prefix('teachers')->middleware(['role:superadmin,admin', 'auth'])->group(function () {
+        Route::get('/', 'index')->name('index');
+        Route::get('/add', 'create')->name('create');
+        Route::get('/{user:id}', 'show')->name('show');
+        Route::post('/', 'store')->name('store');
+        Route::get('/{user:id}/edit', 'edit')->name('edit');
+        Route::put('{user:id}', 'update')->name('update');
+        Route::delete('/delete_all', 'destroyAll')->name('destroy.all');
+        Route::delete('/{id:id}', 'destroy')->name('destroy');
+    });
+
 });
