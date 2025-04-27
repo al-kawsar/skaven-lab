75e4f619c3b0327160e954169c55b3f0c3d04286
Raihan Alkawsar <tlod9906@gmail.com>
2025-04-27 12:00:00 +0800

    fix: update error views for better user feedback

---
+++ resources/views/errors/429.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+    <title>Lab Manajemen - Error 429</title>
+
+    <link rel="shortcut icon" href="/assets/img/favicon.png">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+
+    <link rel="stylesheet" href="/assets/plugins/bootstrap/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="/assets/plugins/feather/feather.css">
+
+    <link rel="stylesheet" href="/assets/plugins/icons/flags/flags.css">
+
+    <link rel="stylesheet" href="/assets/plugins/fontawesome/css/fontawesome.min.css">
+    <link rel="stylesheet" href="/assets/plugins/fontawesome/css/all.min.css">
+
+    <link rel="stylesheet" href="/assets/css/style.css">
+</head>
+
+<body class="error-page">
+
+    <div class="main-wrapper">
+        <div class="error-box">
+            <h1>429</h1>
+            <h3 class="h2 mb-3"><i class="fas fa-exclamation-triangle"></i> Ups! Terlalu Banyak Permintaan!</h3>
+            <p class="h4 font-weight-normal">Anda telah melakukan terlalu banyak permintaan. Silakan coba lagi nanti.
+            </p>
+            <a href="{{ url()->previous() }}" class="btn btn-primary">Back to Home</a>
+        </div>
+    </div>
+
+
+    <script src="/assets/js/jquery-3.6.0.min.js"></script>
+
+    <script src="/assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <script src="/assets/js/script.js"></script>
+</body>
+
+</html>
